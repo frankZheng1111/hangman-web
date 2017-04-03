21e0a55c36b9d400673fd0c967b5e2163cebc8b2
frankZheng1111 <DingYuan.Zheng1111@hotmail.com>
2017-04-03 18:32:34 +0800

    minor changes

--- src/libs/logger.js
+++ src/libs/logger.js
@@ -1,9 +1,9 @@
-"use strict"
-import log4js from "log4js";
+'use strict'
+import log4js from 'log4js';
 import { logger as logConfig } from '../config';
 
 log4js.configure(logConfig);
 
-let logger = log4js.getLogger("normal");
+let logger = log4js.getLogger('normal');
 
 export default logger;
