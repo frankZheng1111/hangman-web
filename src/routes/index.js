3b8aab471b688704a73133281e737cf466485b59
frankZheng1111 <DingYuan.Zheng1111@hotmail.com>
2017-04-09 22:01:28 +0800

    Add defend routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,8 +2,14 @@
 import hangmen from './hangmen';
 import users from './users';
 export default function (app) {
+  app.get('/', (req, res) => {
+    res.redirect('/hangmen');
+  });
   app.use('/hangmen', hangmen);
   app.use('/users', users);
+  app.get('*', (req, res) => {
+    res.redirect('/hangmen');
+  });
   app.use((req, res) => {
     res.status(404).render('404',  { layout: false });
   });
