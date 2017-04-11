fa97af70b080ad80c5b36cb5434286d5a4330f5c
frankZheng1111 <dingyuan.zheng1111@hotmail.com>
2017-04-11 17:28:13 +0800

    change routes sort to improve code

--- src/routes/hangmen.js
+++ src/routes/hangmen.js
@@ -1,13 +1,12 @@
 'use strict'
 import express from 'express';
 import Hangman from '../models/hangmen'
-import {  checkLogin } from '../middlewares/check';
 
 const router = express.Router();
 
 // 获取所有或指定用户的猜过的记录
 //
-router.get('/', checkLogin, (req, res, next) => {
+router.get('/', (req, res, next) => {
   Hangman.findAllByPlayer(req.session.user._id, 1, 2).then((hangmen) => {
     res.render('./hangmen/index', { hangmen: hangmen });
   }).catch(next);
@@ -15,7 +14,7 @@ router.get('/', checkLogin, (req, res, next) => {
 
 // 查看单条记录的详情
 //
-router.get('/:id', checkLogin, (req, res, next) => {
+router.get('/:id', (req, res, next) => {
   let hangman = Hangman.findById(req.params.id).then((hangman) => {
     if (!hangman) { throw new Error('hangman not exist'); }
     const LETTERS = 'abcdefghijklmnopqrstuvwxyz-'.split('');
@@ -25,7 +24,7 @@ router.get('/:id', checkLogin, (req, res, next) => {
 
 // 开始一次猜词(创建一条记录)
 //
-router.post('/', checkLogin, (req, res, next) => {
+router.post('/', (req, res, next) => {
   Hangman.newGame(req.session.user).then((hangman) => {
     res.redirect(`/hangmen/${hangman._id}`)
   }).catch(next);
@@ -33,7 +32,7 @@ router.post('/', checkLogin, (req, res, next) => {
 
 // 猜一个字母
 //
-router.patch('/:id', checkLogin, (req, res, next) => {
+router.patch('/:id', (req, res, next) => {
   let hangman = Hangman.findById(req.params.id).then((hangman) => {
     if (!hangman) { throw new Error('hangman not exist'); }
     if (hangman.isFinished()) { return Promise.resolve(hangman); }
@@ -45,7 +44,7 @@ router.patch('/:id', checkLogin, (req, res, next) => {
 
 // 这轮游戏弃权
 //
-router.patch('/:id/giveup', checkLogin, (req, res, next) => {
+router.patch('/:id/giveup', (req, res, next) => {
   let hangman = Hangman.findById(req.params.id).then((hangman) => {
     if (!hangman) { throw new Error('hangman not exist'); }
     if (hangman.isFinished()) { return Promise.resolve(hangman); }
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,14 @@
 'use strict'
+import {  checkLogin } from '../middlewares/check';
 import hangmen from './hangmen';
 import users from './users';
 export default function (app) {
   app.get('/', (req, res) => {
     res.redirect('/users/signin');
   });
-  app.use('/hangmen', hangmen);
   app.use('/users', users);
+  app.use(checkLogin);
+  app.use('/hangmen', hangmen);
   app.get('*', (req, res) => {
     res.redirect('/hangmen');
   });
