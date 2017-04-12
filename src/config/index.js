8df71c466d2982de1b3bc1e9a82650e36d8d47fc
frankZheng1111 <dingyuan.zheng1111@hotmail.com>
2017-04-12 22:45:58 +0800

    Update config

--- src/config/index.js
+++ src/config/index.js
@@ -3,5 +3,5 @@ const ENV = process.env.NODE_ENV || 'dev'
 
 export const server = require('../../config/server.json')[ENV]
 export const mongo = require('../../config/mongo.json')[ENV]
-export const logger = require('../../config/logger.json')[ENV]
+export const logger = require('../../config/logger.json')
 export const session = require('../../config/session.json')[ENV]
