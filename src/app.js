1b1cdd4dc0a98d6643d3187f12870cb1c61eb81a
frankZheng1111 <dingyuan.zheng1111@hotmail.com>
2017-05-01 14:49:02 +0800

    minor change

--- src/app.js
+++ src/app.js
@@ -54,18 +54,18 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use(methodOverride((req, res) => {
-    if (req.body && typeof req.body === 'object' && '_method' in req.body) {
-      // look in urlencoded POST bodies and delete it
-      let method = req.body._method;
-      delete req.body._method;
-      return method;
-    }
-}))
+app.use(methodOverride((req) => {
+  if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+    // look in urlencoded POST bodies and delete it
+    let method = req.body._method;
+    delete req.body._method;
+    return method;
+  }
+}));
 
 routes(app);
 
-app.use((err, req, res, next) => {
+app.use((err, req, res) => {
   logger.error(err);
   res.render('500', {
     layout: false,
